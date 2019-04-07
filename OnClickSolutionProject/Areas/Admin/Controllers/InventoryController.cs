bd799a5973b89205112e39ebe1e35f897be8ba63
Mahmoud Barati Mosleh <mahmoud.b1366@gmail.com>
2019-04-07 17:53:40 -0400

    Commiting Ali's Code to Repo - Inventory Added

---
+++ OnClickSolutionProject/Areas/Admin/Controllers/InventoryController.cs
@@ -0,0 +1,44 @@
+﻿using OnClickSolution.Data;
+using OnClickSolution.Models;
+using OnClickSolution.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using OnClickSolution.Models.ViewModel;
+
+namespace OnClickSolution.Areas.Customer.Controllers
+{
+    [Authorize(Roles = SD.SuperAdminEndUser + "," + SD.AdminEndUser)]
+
+    [Area("Admin")]
+
+    public class InventoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly HostingEnvironment _hostingEnvironment;
+
+        [BindProperty]
+        public ProductsViewModel ProductsVM { get; set; }
+
+        public InventoryController(ApplicationDbContext db, HostingEnvironment hostingEnvironment)
+        {
+            _db = db;
+            _hostingEnvironment = hostingEnvironment;
+            ProductsVM = new ProductsViewModel()
+            {
+                Categories = _db.Categories.ToList(),
+                Products = new Models.Products()
+            };
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var product = _db.Products.Include(m => m.Categories);
+            return View(await product.ToListAsync());
+        }
+    }
+}
